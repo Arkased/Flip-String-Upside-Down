93c6a23d42d616d931e41f7e4485ca22e69fedef
Kevin Liu <kevintmliu@gmail.com>
2019-03-25 21:14:03 -0700

    added comments and improved readability

--- flip_string.py
+++ flip_string.py
@@ -6,174 +6,21 @@ Takes a text file and flips it upside down
 @author: kevin
 """
 import codecs
-inp = """a
-b
-c
-d
-e
-f
-g
-h
-i
-j
-k
-l
-m
-n
-o
-p
-q
-r
-s
-t
-u
-v
-w
-x
-y
-z
-A
-B
-C
-D
-E
-F
-G
-H
-I
-J
-K
-L
-M
-N
-O
-P
-Q
-R
-S
-T
-U
-V
-W
-X
-Y
-Z
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-,
-.
-?
-!
-,
-'
-`
-(
-)
-[
-]
-{
-}
-<
->
-&"""
-
-out = """ɐ
-q
-ɔ
-p
-ǝ
-ɟ
-ƃ
-ɥ
-ᴉ
-ɾ
-ʞ
-l
-ɯ
-u
-o
-d
-b
-ɹ
-s
-ʇ
-n
-ʌ
-ʍ
-x
-ʎ
-z
-∀
-B
-Ɔ
-D
-Ǝ
-Ⅎ
-פ
-H
-I
-ſ
-K
-˥
-W
-N
-O
-Ԁ
-Q
-R
-S
-┴
-∩
-Λ
-M
-X
-⅄
-Z
-0
-Ɩ
-ᄅ
-Ɛ
-ㄣ
-ϛ
-9
-ㄥ
-8
-6
-'
-˙
-¿
-¡
-,
-,
-,
-)
-(
-]
-[
-}
-{
->
-<
-&"""
-inp = inp.split('\n')
-out = out.split('\n')
+inp = '[\'a\', \'b\', \'c\', \'d\', \'e\', \'f\', \'g\', \'h\', \'i\', \'j\', \'k\', \'l\', \'m\', \'n\', \'o\', \'p\', \'q\', \'r\', \'s\', \'t\', \'u\', \'v\', \'w\', \'x\', \'y\', \'z\', \'A\', \'B\', \'C\', \'D\', \'E\', \'F\', \'G\', \'H\', \'I\', \'J\', \'K\', \'L\', \'M\', \'N\', \'O\', \'P\', \'Q\', \'R\', \'S\', \'T\', \'U\', \'V\', \'W\', \'X\', \'Y\', \'Z\', \'0\', \'1\', \'2\', \'3\', \'4\', \'5\', \'6\', \'7\', \'8\', \'9\', \',\', \'.\', \'?\', \'!\', \',\', "\'", \'`\', \'(\', \')\', \'[\', \']\', \'{\', \'}\', \'<\', \'>\', \'&\']'
+out = '[\'ɐ\', \'q\', \'ɔ\', \'p\', \'ǝ\', \'ɟ\', \'ƃ\', \'ɥ\', \'ᴉ\', \'ɾ\', \'ʞ\', \'l\', \'ɯ\', \'u\', \'o\', \'d\', \'b\', \'ɹ\', \'s\', \'ʇ\', \'n\', \'ʌ\', \'ʍ\', \'x\', \'ʎ\', \'z\', \'∀\', \'B\', \'Ɔ\', \'D\', \'Ǝ\', \'Ⅎ\', \'פ\', \'H\', \'I\', \'ſ\', \'K\', \'˥\', \'W\', \'N\', \'O\', \'Ԁ\', \'Q\', \'R\', \'S\', \'┴\', \'∩\', \'Λ\', \'M\', \'X\', \'⅄\', \'Z\', \'0\', \'Ɩ\', \'ᄅ\', \'Ɛ\', \'ㄣ\', \'ϛ\', \'9\', \'ㄥ\', \'8\', \'6\', "\'", \'˙\', \'¿\', \'¡\', \',\', \',\', \',\', \')\', \'(\', \']\', \'[\', \'}\', \'{\', \'>\', \'<\', \'&\']'
 flip = {inp[i]:out[i] for i in range(len(inp))}
 
-def open_file(file = 'test_input.txt'):
+def read_text_from_file(file = 'test_input.txt'):
+    """Opens a (.txt) file and returns its contents.
+    """
     file = open(file, 'r', errors = 'ignore', encoding = 'utf-8')
     text = file.read()
     file.close()
     return text
 
 def flip_text(text):
+    """Flips a string upside down.
+    """
     flipped_text = ''
     for char in text:
         if char in flip:
@@ -183,6 +30,9 @@ def flip_text(text):
     return flipped_text
 
 def save_text(text, filename = 'test_output.txt'):
+    """Writes a string to a new file. I ran into some issues with encoding if I
+    saved directly as a .docx file (Unicode characters of upside down lettersdon't render properly)
+    """
     file = codecs.open(filename, 'w', 'utf-8')
     file.write(text)
     file.close()
